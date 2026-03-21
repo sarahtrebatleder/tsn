9c9bd0257453924ebf7dbe00308e1e27b3702365
sarahtrebatleder <sarahtrebatleder@gmail.com>
2026-03-21 17:13:52 -0400

    Create config.js

---
+++ config.js
@@ -0,0 +1,11 @@
+const CONFIG = {
+  FIREBASE: {
+    apiKey:           "AIzaSyAC9TYmf9s9gbw2J8D6wG2tzKvH6IZUbz4",
+    authDomain:        "tsn-app-eef65.firebaseapp.com",
+    projectId:         "tsn-app-eef65",
+    storageBucket:     "tsn-app-eef65.firebasestorage.app",  
+    messagingSenderId: "499985499202",  
+    appId:             "1:499985499202:web:df67f761edccff974099db",
+  },
+  GOOGLE_MAPS_API_KEY: "AIzaSyDrDWSRknK8At_WgB_ZmC0pvWVKXf7FLxc",
+};
